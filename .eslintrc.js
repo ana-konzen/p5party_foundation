2626d96b155aafd9defe4a4bb01b3dee561b165a
Justin Bakse <justin@tsfim.com>
2022-11-03 22:02:31 -0500

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "p5js"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
